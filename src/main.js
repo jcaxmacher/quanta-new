73dacd2fde93f30041553bb539e4f485a2f3f0a0
Jeremy Axmacher <jeremy@obsoleter.com>
2014-04-17 17:03:40 -0400

    rough sketch of data model

--- src/main.js
+++ src/main.js
@@ -3,6 +3,34 @@ var Vue = require('vue');
 new Vue({
     el: 'body',
     data: {
-        msg: 'Hello Vue.js!'
+        msg: 'Hello Vue.js!',
+        logs: [],
+        logTypes: [{
+            name: 'start',
+            fields: [
+                'time',
+                'name'
+            ]
+        }, {
+            name: 'stop',
+            fields: [
+                'time',
+                'name'
+        }, {
+            name: 'count',
+            fields: [
+                'name',
+                'description',
+                'value',
+                'time',
+                'attachments'
+        }, {
+            name: 'memory',
+            fields: [
+                'name',
+                'description',
+                'time',
+                'attachments'
+        }]
     }
 });
